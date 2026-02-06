7dea6c48746950d89afdc97340b7d9903d8a9f3a
sertackesel110-cmd <sertackesel110@gmail.com>
2026-02-06 10:23:16 +0300

    Update ExampleMixin.java

--- src/main/java/com/example/addon/mixin/ExampleMixin.java
+++ src/main/java/com/example/addon/mixin/ExampleMixin.java
@@ -2,15 +2,8 @@ package com.example.addon.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import com.example.addon.AddonTemplate;
 
 @Mixin(TitleScreen.class)
 public class ExampleMixin {
-    @Inject(at = @At("HEAD"), method = "init()V")
-    private void onInit(CallbackInfo info) {
-        AddonTemplate.LOG.info("Mixin initialized successfully for 1.21.4");
-    }
+    // Empty class to prevent startup crashes and black screen
 }
