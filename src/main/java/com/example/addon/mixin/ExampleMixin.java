58e880abae416ddb5ca733cb4550c49a0ab24117
sertackesel110-cmd <sertackesel110@gmail.com>
2026-02-06 11:40:44 +0300

    Delete src/main/java/com/example/addon/mixin directory

--- src/main/java/com/example/addon/mixin/ExampleMixin.java
+++
@@ -1,8 +0,0 @@
-package com.example.addon.mixin;
-
-import org.spongepowered.asm.mixin.Mixin;
-import net.minecraft.client.gui.screen.TitleScreen;
-
-@Mixin(TitleScreen.class)
-public class ExampleMixin {
-}
